dc11fc2aa32c17dbd0e8965cae4e80e8b40003ab
chanukaaaa <it21281082@my.sliit.lk>
2024-01-17 23:01:14 +0530

    Right side in progress

--- convo-ai/src/Components/Main/Main.jsx
+++ convo-ai/src/Components/Main/Main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Main.css'
+import sendBtn from '../Assets/send.svg'
+import userIcon from '../Assets/user2.png'
+import logo2 from '../Assets/logo2.png'
 
 const Main = () => {
   return (
     <div className='main'>
-      
+      <div className="chats">
+        <div className="chat">
+          <img src={userIcon} alt="" /><p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae distinctio sint possimus dolores repellat laboriosam aspernatur fugit quas ratione doloribus.</p>
+        </div>
+        <div className="chat">
+          <img src={logo2} alt="" /><p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae distinctio sint possimus dolores repellat laboriosam aspernatur fugit quas ratione doloribus.</p>
+        </div>
+      </div>
+      <div className="chatFooter">
+        <div className="inp">
+          <input type="text"  placeholder='Chat with ConvoAI...'/> <button className="send"><img src={sendBtn} alt="send " /></button>
+        </div>
+      </div>
     </div>
   )
 }
